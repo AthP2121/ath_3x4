356eb4452a2f5c5b191ce89544a3013ed35b045b
AthP2121 <37655992+AthP2121@users.noreply.github.com>
2023-10-19 16:50:45 +1000

    Add files via upload
    Uploading the firmware configs for QMK, VIA and VIAL

---
+++ ath/ath_3x4/keymaps/via/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x74, 0xE5, 0xEC, 0xB2, 0xC2, 0x14, 0xFF, 0xB9}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
